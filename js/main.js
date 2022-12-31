d1b66ca43f5cf8e9ca6f3b8452dc442c99bcdb2a
Silvia0307 <sonorous0307@gmail.com>
2022-12-31 18:59:41 +0900

    navbar toggle 기능 추가
    nav의 bar 클릭했을 때 메뉴  toggle 기능 추가

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 //scroll할 때 navbar background color 변경
+// Element.getBoundingClientRect()
+// 엘리먼트의 크기와 뷰포트에 상대적인 위치 정보를 제공하는 DOMRect 객체를 반환
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
@@ -9,5 +11,14 @@ document.addEventListener('scroll', () => {
     }
 });
 
-// Element.getBoundingClientRect()
-// 엘리먼트의 크기와 뷰포트에 상대적인 위치 정보를 제공하는 DOMRect 객체를 반환
+// menu toggle 기능
+const navbarToggle = document.querySelector('.toggle');
+const navbarMenu = document.querySelector('.navbar_menu');
+const navbarLink = document.querySelector('.navbar_links');
+navbarToggle.addEventListener('click',()=>{
+    navbarToggle.classList.toggle('active');
+    navbarMenu.classList.toggle('active');
+    navbarLink.classList.toggle('active');
+});
+
+
